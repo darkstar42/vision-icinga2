f07253c619f40931448b0e5c1b90e10b2137d559
Michael Haase <haaseml@iis.fraunhofer.de>
2016-07-26 16:55:02 +0200

    Add Icinga2 object config

--- manifests/client.pp
+++ manifests/client.pp
@@ -38,8 +38,9 @@ class vision_icinga2::client (
       ['=', 'title', 'Vision_icinga2::Client'],
       ['=', ['parameter', 'parent_zone'], $::fqdn]]);
 
-
   each ($child_nodes) |$child_node| {
+    notify { "${child_node}": }
+
     $child_params = $child_node['parameters']
 
     ::icinga2::object::zone { $child_node['title']:
