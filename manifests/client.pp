97161cc51514ce05717aa8a8c5f453334f546783
Michael Haase <haaseml@iis.fraunhofer.de>
2016-07-26 15:32:54 +0200

    Fix Icinga2

--- manifests/client.pp
+++ manifests/client.pp
@@ -18,19 +18,11 @@ class vision_icinga2::client (
 
   contain ::vision_icinga2::common::features
 
-  ::icinga2::object::endpoint { $::fqdn:
-    host => $::fqdn,
-  }
-
   ::icinga2::object::zone { $::fqdn:
     parent    => $parent_zone,
     endpoints => {
       $::fqdn => {}
     }
-  }
-
-  ::icinga2::object::endpoint { $parent_zone:
-    host => $parent_zone
   }
 
   ::icinga2::object::zone { $parent_zone:
