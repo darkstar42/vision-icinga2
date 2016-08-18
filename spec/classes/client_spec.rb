fc795b99d76320a18743169810489dce8b329b84
Ulli Goschler <goschlui@iis.fraunhofer.de>
2016-08-18 10:19:58 +0200

    add specs for client and server

---
+++ spec/classes/client_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'hiera'
+
+describe 'vision_icinga2::client' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      before(:each) do
+        Puppet::Parser::Functions.newfunction(:query_resources,
+                                              :type => :rvalue) { |args|
+          return []
+        }
+      end
+
+      context 'compile' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_user('nagios') }
+        it { is_expected.to contain_user('icinga') }
+      end
+
+    end
+  end
+end
