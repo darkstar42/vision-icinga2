c582c067bb72de04f4f486df6e5c59a9a62ae0a4
Ulli Goschler <goschlui@iis.fraunhofer.de>
2016-08-30 16:48:04 +0200

    move all parameters to init location

--- manifests/server.pp
+++ manifests/server.pp
@@ -15,15 +15,18 @@ class vision_icinga2::server (
   String $api_user,
   String $api_password,
 
+  String $mysql_user,
+  String $mysql_password,
+  String $mysql_database,
+  String $mysql_root_password,
+
 ) {
   contain ::vision_icinga2::common::install
+  contain ::vision_icinga2::server::install
 
   contain ::vision_icinga2::common::features
   contain ::vision_icinga2::server::features
 
   contain ::vision_icinga2::common::object
-  class { '::vision_icinga2::server::object':
-    api_user     => $api_user,
-    api_password => $api_password,
-  }
+  contain ::vision_icinga2::server::object
 }
