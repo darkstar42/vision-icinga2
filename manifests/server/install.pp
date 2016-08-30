a3248f548d596b0fcacb53fbc6d674362efd5c51
Ulli Goschler <goschlui@iis.fraunhofer.de>
2016-08-30 16:49:53 +0200

    seperate client/server install to appr. folders

---
+++ manifests/server/install.pp
@@ -0,0 +1,45 @@
+#
+class vision_icinga2::server::install () {
+
+  class { '::vision_mysql::server':
+    root_password => $::vision_icinga2::server::mysql_root_password,
+  }
+
+  ::mysql::db { $::vision_icinga2::server::mysql_database:
+    user     => $::vision_icinga2::server::mysql_user,
+    password => $::vision_icinga2::server::mysql_password,
+    host     => '%',
+    grant    => [
+      'SELECT',
+      'UPDATE',
+      'CREATE',
+      'INSERT',
+      'INDEX',
+      'ALTER',
+      'DELETE',
+      'DROP',
+    ],
+  }
+
+  class { '::icinga2':
+    db_type         => 'mysql',
+    db_host         => 'localhost',
+    db_port         => '3306',
+    db_name         => $::vision_icinga2::server::mysql_database,
+    db_user         => $::vision_icinga2::server::mysql_user,
+    db_pass         => $::vision_icinga2::server::mysql_password,
+    use_debmon_repo => false,
+    manage_database => false,
+    purge_configs   => true,
+    purge_confd     => true,
+    require         => Mysql::Db[$::vision_icinga2::server::mysql_database],
+  }
+
+  class { 'vision_icinga2::server::feature::ido_mysql':
+    host     => 'localhost',
+    port     => '3306',
+    user     => $::vision_icinga2::server::mysql_user,
+    password => $::vision_icinga2::server::mysql_password,
+    database => $::vision_icinga2::server::mysql_database,
+  }
+}
