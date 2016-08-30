5295b13f65be21324e73195b8b90d56ed36dc4db
Ulli Goschler <goschlui@iis.fraunhofer.de>
2016-08-30 16:50:46 +0200

    add dummy cert generation script

---
+++ files/testing/gencrt.sh
@@ -0,0 +1,7 @@
+mkdir -p /vision/pki/
+openssl genrsa -out /tmp/dummyCA.key 1024
+openssl req -x509 -new -nodes -key /tmp/dummyCA.key -sha256 -days 1 -out /tmp/dummyCA.pem -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com"
+
+openssl genrsa -out /tmp/dummycert.key 1024
+openssl req -new -key /tmp/dummycert.key -out /tmp/dummycert.csr -subj "/C=GB/ST=No/L=London/O=Security/OU=foobar/CN=`/opt/puppetlabs/bin/facter fqdn`"
+openssl x509 -req -in /tmp/dummycert.csr -CA /tmp/dummyCA.pem -CAkey /tmp/dummyCA.key -CAcreateserial -out /tmp/dummycert.crt -days 1
