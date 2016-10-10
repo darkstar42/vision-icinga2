a6987beea419c8ff05706374b24b1e3faaf1bfb4
Michael Haase <haaseml@iis.fraunhofer.de>
2016-10-10 12:27:31 +0200

    Update parent host dependency

--- manifests/common/object/dependency.pp
+++ manifests/common/object/dependency.pp
@@ -1,9 +1,3 @@
 # documentation
 class vision_icinga2::common::object::dependency {
-  ::icinga2::object::apply_dependency { 'parent':
-    parent_host_name      => '$host.vars.parent$',
-    assign_where          => 'host.address && host.vars.parent',
-    disable_notifications => true,
-    disable_checks        => true
-  }
 }
